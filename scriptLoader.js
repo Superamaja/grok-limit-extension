bcbf768fe3e34ac1eb51b86356d059c91959f294
Connor Lin <connorlin06@gmail.com>
2025-03-06 18:25:19 -0600

    chore(rateLimit): remove unused rate limit monitoring scripts

--- scriptLoader.js
+++
@@ -1,4 +0,0 @@
-const script = document.createElement("script");
-script.src = chrome.runtime.getURL("grokRateLimitDetector.js");
-script.type = "text/javascript";
-document.head.appendChild(script);
